ce2ac12f1b7f79a75b2f0b07425d0d1245c33419
0jpancho <31480952+0jpancho@users.noreply.github.com>
2017-11-08 10:01:40 -0600

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -4,8 +4,6 @@ function Pixel (r, g, b) {
   this.green = g;
   this.blue = b;
 
-  let rgb = [];
-
   this.addRed = function(n){
     this.red = this.red + n;
 
@@ -46,6 +44,30 @@ function Pixel (r, g, b) {
      this.red = this.red + (this.red * n);
      this.green = this.green + (this.green * n);
      this.blue = this.blue + (this.blue * n);
+
+     if (this.red < 0){
+         this.red = 0;
+     }
+
+     else if (this.red > 255){
+         this.red = 255;
+     }
+
+     if (this.green < 0){
+         this.green = 0;
+     }
+
+     else if (this.green > 255){
+          this.green = 255;
+     }
+
+     if (this.blue < 0){
+         this.blue = 0;
+     }
+
+     else if (this.blue > 255){
+         this.blue = 255;
+     }
  };
 
   this.grayScale = function(){
@@ -56,12 +78,20 @@ function Pixel (r, g, b) {
       let compPixel = new Pixel(255 - this.red, 255 - this.green, 255 - this.blue);
       return compPixel;
   };
+
+  this.outputPixel = function(){
+      console.log(this.red, this.green, this.blue);
+
+  }
+  this.outputComplement = function(){
+      console.log(this.complement().red, this.complement().green, this.complement().blue);
+  };
 }
 
 function main(){
     let thingy = new Pixel(50, 100, 150);
-
-    console.log(thingy.complement().red);
+    thingy.outputComplement();
+    thingy.outputPixel();
 
 }
 
